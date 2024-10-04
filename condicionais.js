d09fe718d5d69ad0bd50181d18da0eac851c1aab
Icaro <icaroguilhermecoutinho@gmail.com>
2024-10-04 17:21:15 -0400

    Condicionais
    Hoje aprendi as condicionais,  IF, ELSE, ELSE IF. E estruturas e repetição FOR e WHILE.

---
+++ condicionais.js
@@ -0,0 +1,52 @@
+
+// Exercicio 1
+var numero1 = 100;
+var numero2 = 20;
+
+if(numero1 > numero2){
+    console.log("O primeiro numero e maior.");
+} else{
+    console.log("O segundo numero e maior.");
+}
+
+console.log("");
+
+// Exercicio 2
+var valor = -9;
+
+if( valor > 0){
+    console.log("O valor e positivo.");
+} else if( valor < 0){
+    console.log("O valor e negativo.");
+} else {
+    console.log("O valor e zero");
+}
+
+console.log("");
+
+// Exercicio 3
+var idade = 13;
+
+if( idade >= 18){
+    console.log("Voce ja e de maior.");
+} else {
+    console.log("Voce e de menor.");
+}
+
+console.log("");
+
+//Exercicio 4
+var Celsius = -80; 
+
+var Fahrenheit = Celsius * 1.8 + 32;
+console.log("A conversao para Fahrenheit e:" + Fahrenheit);
+
+console.log("");
+
+//Exercicio 5
+
+var base = 66;
+var altura = 19.4;
+var Area = base * altura;
+
+console.log("A area do retangulo e:" + Area);
---
+++ estruturas_de_repeticao.js
@@ -0,0 +1,33 @@
+
+//Contagem utilizando o FOR
+var contagem;
+
+for( contagem = 0; contagem < 11; contagem++){
+    console.log("SENAC");
+}
+
+console.log("");
+
+for ( var i = 0; i <= 5; i++){
+    console.log("Numero:" + i);
+}
+ 
+console.log("");
+
+var contador = 0;
+
+while(contador <= 10){
+    console.log(contador);
+    contador++;
+}
+
+console.log("");
+
+//Exercicio 1
+
+var n = 15;
+var soma = 0;
+for (let i = 1; 1 <= n; i++){
+    soma += i;
+    console.log(soma);
+}
