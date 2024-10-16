5896b52f0e1e5a70c412373819c6be80a6ae6953
Icaro <icaroguilhermecoutinho@gmail.com>
2024-10-16 16:35:01 -0400

    Melhora Final

--- jucao/index.js
+++ jucao/index.js
@@ -1,7 +1,8 @@
 var botao = document.getElementById('btn');
-const salver = document.getElementById('send')
+const salver = document.getElementById('send');
+
 botao.addEventListener("click", function(){
-    alert('Be careful, The button has been pressed, lazybones found.');
+    alert('We are facing a significant increase in the spread of a new virus that could compromise the security of your devices and personal data. This virus mainly spreads through suspicious emails, malicious links and downloads from untrustworthy sources.');
 });
 
 salver.addEventListener('click', function(e){
