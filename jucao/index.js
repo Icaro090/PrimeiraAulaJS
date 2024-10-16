916aa4fa293dc38b383f224277642645d284ac3d
Icaro <icaroguilhermecoutinho@gmail.com>
2024-10-15 21:11:51 -0300

    Semitérmino

--- jucao/index.js
+++ jucao/index.js
@@ -1,6 +1,14 @@
 var botao = document.getElementById('btn');
-
+const salver = document.getElementById('send')
 botao.addEventListener("click", function(){
     alert('Be careful, The button has been pressed, lazybones found.');
 });
 
+salver.addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    const name = document.getElementById('textoss');
+    const value = name.value;
+    document.getElementById('texto-frente').innerHTML = value;
+});
